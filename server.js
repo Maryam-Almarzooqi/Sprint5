960406e8b6baffaebad639cafc7e1b9aefdff0e4
Maryam-Almarzooqi <g00095349@aus.edu>
2025-04-27 20:41:32 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
+app.use(cors());
 
 // Temporary in-memory storage
 let tasks = [];
